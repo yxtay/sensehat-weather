8c1c29609c586007ad7d823b53d2b356d0c2484c
yxtay <wyextay@gmail.com>
2017-07-11 20:10:50 +0800

    edits logger and uses argparse

--- sensehat_weather.py
+++ sensehat_weather.py
@@ -1,33 +1,20 @@
-from __future__ import print_function
-
+from argparse import ArgumentParser
 import datetime
-import logging
-import logging.handlers
+import sys
 import time
 
 from sense_hat import SenseHat
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# configurations to be set accordingly
-GDOCS_OAUTH_JSON = "raspberry-pi-0f26df464c6e.json"
-GDOCS_SPREADSHEET_NAME = "sensehat-weather"
-GDOCS_WORKSHEET_NAME = "data"
-LOG_FILE = "out.log"
+from logger import get_logger
 
 # logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-fh = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=2 ** 20, backupCount=2)
-fh.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-logger.addHandler(fh)
+logger = get_logger(__name__)
 
 
 def get_readings(hat):
     """Get sensor readings and collate them in a dictionary."""
-    logger.debug("getting readings.")
     readings = dict()
     readings["datetime"] = datetime.datetime.now()
     readings["temperature"] = hat.temperature
@@ -35,27 +22,25 @@ def get_readings(hat):
     readings["pressure"] = hat.pressure
 
     # round to 2 decimal places for all readings
-    for key, value in readings.copy().items():
-        if key != "datetime":
-            readings[key] = round(value, 2)
-    logger.debug("completed getting readings.")
+    for key in readings.copy():
+        if key in {"temperature", "humidity", "pressure"}:
+            readings[key] = round(readings[key], 2)
+    logger.debug("readings obtained.")
     return readings
 
 
 def login_open_sheet(oauth_key_file, spreadsheet_name, worksheet_name):
     """Connect to Google Docs spreadsheet and return the first worksheet."""
     try:
-        logger.debug("authenticating google account.")
         scope = ['https://spreadsheets.google.com/feeds']
         credentials = ServiceAccountCredentials.from_json_keyfile_name(oauth_key_file, scope)
         gc = gspread.authorize(credentials)
         logger.debug("google account authorised.")
         worksheet = gc.open(spreadsheet_name).worksheet(worksheet_name)
-        logger.debug("google sheet {} obtained.".format(worksheet_name))
+        logger.debug("google sheet obtained: %s.", worksheet_name)
         return worksheet
 
     except Exception as e:
-        logger.exception(e)
         logger.warning("google sheet login failed. "
                        "check OAuth credentials, spreadsheet name, sheet name"
                        "and make sure spreadsheet is shared to the "
@@ -66,22 +51,35 @@ def login_open_sheet(oauth_key_file, spreadsheet_name, worksheet_name):
 def append_readings(worksheet, readings):
     """Append the data in the spreadsheet, including a timestamp."""
     try:
-        logger.debug("appending readings to {}.".format(worksheet.title))
         columns = ["datetime", "temperature", "humidity", "pressure"]
         worksheet.append_row([readings.get(col, '') for col in columns])
-        logger.debug("appended readings to {}.".format(worksheet.title))
+        logger.debug("readings appended to %s.", worksheet.title)
 
     except Exception as e:
         # Error appending data, most likely because credentials are stale.
         # Null out the worksheet so a login is performed.
-        logger.exception(e)
-        logging.warning("appending error. credentials are probably stale. "
-                        "logging in again.")
+        logger.warning("appending error. credentials are probably stale. "
+                       "logging in again.")
         raise e
 
 
-def main():
-    logger.debug("start of main function.")
+if __name__ == "__main__":
+    arg_parser = ArgumentParser(description="Take readings from SenseHat and add to Google Spreadsheet.")
+    arg_parser.add_argument("--oauth-json", default="credentials.json",
+                            help="path to Google OAuth credentials json file")
+    arg_parser.add_argument("--spreadsheet", default="sensehat-weather",
+                            help="name of Google Spreadsheet to save SenseHat readings")
+    arg_parser.add_argument("--worksheet", default="data",
+                            help="name of worksheet to save SenseHat readings")
+    arg_parser.add_argument("--log", default="main.log",
+                            help="path of log file")
+    args = arg_parser.parse_args()
+
+    logger = get_logger(__name__, args.log)
+    logger.debug("call: %s.", " ".join(sys.argv))
+    logger.debug("ArgumentParser: %s.", args)
+
+    # connect sense hat
     hat = SenseHat()
     hat.clear()
     logger.debug("sense hat connected.")
@@ -96,16 +94,10 @@ def main():
     # upload to google sheet
     for _ in range(30):
         try:
-            worksheet = login_open_sheet(GDOCS_OAUTH_JSON, GDOCS_SPREADSHEET_NAME, GDOCS_WORKSHEET_NAME)
+            worksheet = login_open_sheet(args.oauth_json, args.spreadsheet, args.worksheet)
             append_readings(worksheet, readings)
             break
         except Exception as e:
-            logger.warning(e)
+            logger.exception(e)
             time.sleep(120)
             continue
-
-    logger.debug("end of main function.")
-
-
-if __name__ == "__main__":
-    main()
